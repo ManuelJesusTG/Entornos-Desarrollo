d2f6bd319b02ae7def09daf899c39550eeea9e5e
ManuelJesusTG <alum.mjesusterrong@iesalixar.org>
2021-10-04 13:20:42 +0200

    Colores Definidos1

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,14 @@
-
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!
+ *
+ *@authot ManuelJesusTG
+ */
 public class HolaMundo {
 	public static void main(String[] args) {
 	String naranja = "\033[33m";
 	String azul = "\033[34m";
+	
 		System.out.println("¡Hola mundo!");
-		}
+		System.out.println("Aquí estamos haciendo pruebas con Git.");
 	}
+}
